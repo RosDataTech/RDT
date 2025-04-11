cefc2308fa00a810195563a2eb0a975a357aec2a
LeoIzotov <132166252+LeoIzotov@users.noreply.github.com>
2025-04-11 13:17:37 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -4,54 +4,25 @@ import rdt_database as db
 import rdt_ml as ml
 
 def main():
-    # Ввод запроса от пользователя
-    hh_search_term = str(input("Введите название профессии: ").strip())
+    hh_search_term = ''
 
-    # Получение требований из hh.ru
-    print(f"\nПолучение требований по вакансии '{hh_search_term}'...")
-    job_texts = hh.job_req_search(hh_search_term, exp_level)
+    job_texts = hh.job_req_search(hh_search_term)
 
-    # Загрузка образовательных программ из базы
     db_url = 'Your database url here'
-    uni_data = db.dataToDict(db_url)
+    uni_data = db.dataToDict(db_url)['IT']
 
-    # Извлечение текстов программ обучения
-    edu_texts = ""
+    edu_texts = []
     for uni_name, pdf_url in uni_data.items():
         try:
-            edu_texts += "\n" + pdf.uni_cutter(pdf_url, uni_name)
+            edu_texts.append(pdf.uni_cutter(pdf_url, uni_name))
         except Exception as e:
             print(f"Не удалось обработать {uni_name}: {e}")
 
-    # Выбор метрики сравнения
-    metrics = ['optimal', 'cluster', 'semantic']
-    default_metric = 'optimal'
+    metric = ''  # default_metric = 'optimal', ['optimal', 'cluster', 'semantic']
 
-    while True:
-        metric = input(
-            f"Выберите метрику сравнения из доступного списка:\n"
-            f" - optimal (по умолчанию)\n"
-            f" - cluster\n"
-            f" - semantic\n"
-            f"Нажмите Enter для выбора default_metric = optimal\n"
-            f"Ваш выбор: "
-        ).strip().lower()
-
-        if not metric:
-            metric = default_metric
-            print(f"\nИспользуется метрика по умолчанию: '{metric}'\n")
-            break
-
-        if metric not in metrics:
-            print("\nОшибка: такого значения нет в списке. Попробуйте ещё раз.\n")
-        else:
-            print(f"\nИспользуется метрика: '{metric}'\n")
-            break
-
-
-    # Анализ соответствия
     analyzer = ml.Analyzer()
-    result = analyzer.main_analyze(edu_texts, job_texts, metric)
+    for edu_text, job_text in edu_texts, job_texts:
+        result = analyzer.main_analyze(edu_text, job_text, metric) # не закончил
 
     print("\nКоэффициент соответствия:")
     print(result)
